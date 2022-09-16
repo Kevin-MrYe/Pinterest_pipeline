8b270db8ba302316039353d13140531b5919b812
kevin <mrbinye@gmail.com>
2022-09-17 00:38:05 +0100

    update api

--- api/project_pin_API.py
+++ api/project_pin_API.py
@@ -5,11 +5,28 @@ import uvicorn
 from json import dumps
 from kafka import KafkaProducer
 
+#Create an instance of FastAPI
 app = FastAPI()
 
-
-
 class Data(BaseModel):
+    """
+    Pydantic model to cast parameters to the correct type
+
+    Attributes:
+        category (str): The category of the entry
+        index (int): The index of the entry
+        unique_id (str): The unique id of the entry
+        title (str): The title of the entry
+        description (str): The title of the entry
+        poster_name (str): The title of the entry
+        follower_count (str): The number of followers of the entry
+        tag_list (str): The tags of the entry
+        image_or_video (str): The lable indicating whether entry is image or video
+        image_src (str): The image urls of the entry
+        downloaded (int): The number containing 0(not downloaded) or 1(downloaded)
+        save_location (str): The save location of where the entry saved
+
+    """
     category: str
     index: int
     unique_id: str
@@ -23,12 +40,14 @@ class Data(BaseModel):
     downloaded: int
     save_location: str
 
+# Create Producer to send message to a kafka topic
 td_producer = KafkaProducer(
     bootstrap_servers="localhost:9092",
     client_id="Pinterest data producer",
     value_serializer=lambda mlmessage: dumps(mlmessage).encode("ascii")
 ) 
 
+# An API execute some actions when a post request to localhost:9092/pin/
 @app.post("/pin/")
 def get_db_row(item: Data):
     data = dict(item)
